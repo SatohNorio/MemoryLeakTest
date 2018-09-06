62cac78624ef98a65e22e7cf96661cf0cc9e688a
NorioletsWin8\n-sato <n-sato@okd.intra>
2018-09-06 21:09:16 +0900

    新規作成

---
+++ MemoryLeakTest/DummyRecord.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace MemoryLeakTest
+{
+	public class DummyRecord
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		private static int RefCount;
+
+		public DummyRecord()
+		{
+			Interlocked.Increment(ref DummyRecord.RefCount);
+			Debug.WriteLine($"{nameof(DummyRecord)} Hash:{this.GetHashCode():X} {DummyRecord.RefCount}件↗");
+		}
+
+		~DummyRecord()
+		{
+			Interlocked.Decrement(ref DummyRecord.RefCount);
+			Debug.WriteLine($"{nameof(DummyRecord)} Hash:{this.GetHashCode():X} {DummyRecord.RefCount}件↘");
+		}
+	}
+}
---
+++ MemoryLeakTest/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryLeakTest
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string name)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		private ObservableCollection<DummyRecord> FItems = new ObservableCollection<DummyRecord>();
+
+		public ObservableCollection<DummyRecord> Items
+		{
+			get => this.FItems;
+			set
+			{
+				if (this.FItems != value)
+				{
+					this.FItems = value;
+					this.OnPropertyChanged(nameof(this.Items));
+				}
+			}
+		}
+
+		public void AddRecord()
+		{
+			this.Items.Add(new DummyRecord() { Id = this.Items.Count + 1, Name = "Hoge" });
+		}
+
+		public void RemoveRecord()
+		{
+			if (0 < this.Items.Count)
+			{
+				this.Items.RemoveAt(0);
+			}
+			GC.Collect(2);
+		}
+	}
+}
